807a0f0ac0ccc5397fcd6e7191c8edef33e7b4bf
Kevin Andrés Orrego Martínez <51931087+KEVAO18@users.noreply.github.com>
2020-08-23 01:44:47 -0500

    Add files via upload

--- assets/scripts/scr.js
+++ assets/scripts/scr.js
@@ -2,21 +2,35 @@ var user = { 'contar': true };
 $(document).on("ready", preguntas());
 
 function preguntas() {
+  var elem = window.location.search.substring(3);
+  var indice = 100*(elem-1);
+
+  pag_s = document.getElementById('pag-start');
+  pag_b = document.getElementById('pag-foot');
+  for (let ip = 1; ip < 8; ip++) {
+    pag_s.innerHTML +=`<a class='px-4 py-4 text-center text-danger' style='font-size: 1.5rem;' href="?p=`+ip+`">`+ip+`</a>`;
+    pag_b.innerHTML +=`<a class='px-4 py-4 text-center text-danger' style='font-size: 1.5rem;' href="?p=`+ip+`">`+ip+`</a>`;
+  }
+
+
   $.ajax({
     type: "get",
-    url: "https://pokeapi.co/api/v2/pokemon/?limit=101&offset=0",
+    url: "https://pokeapi.co/api/v2/pokemon/?limit=99&offset="+indice+"",
     dataType: "json"
   }).done(
     function (info) {
 
       const p = document.getElementById('contenedor');
       for (let a = 0; a < info.results.length; a++) {
+        indice += 1;
+        if (indice >= 650) {
+          break;
+        }
         p.innerHTML += `
         <div class="col-md-4 col-sm-6 my-4">
           <div class="card py-2 text-center">
             <div class="card-head" style="font-size: 2.5rem;">
-              `
-          +a+ `) ` + info.results[a].name +
+              `+indice+` ` + info.results[a].name +
               `
             </div>
             <div class='card-img-top text-center' id="img`+a+`">
@@ -28,7 +42,6 @@ function preguntas() {
           </div>
         </div>
         `;
-        console.log(info.results[a]);
         $.ajax({
           type: "get",
           url: info.results[a].url,
